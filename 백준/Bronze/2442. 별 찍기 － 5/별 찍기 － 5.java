0714acc1372a049f9d9ac3623e75d0c47cae1710
Ethan <87460980+ethanKim93@users.noreply.github.com>
2023-01-24 23:23:02 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 160 ms, Memory: 16200 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			String answer = "";
+			for (int j = 0; j < N-i-1; j++) {
+				answer += " ";
+			}
+
+			for (int j = N-i; j < N+i+1; j++) {
+				answer += "*";
+			}
+			
+			for (int j = N+i; j < N; j++) {
+				answer += " ";
+			}
+
+			System.out.println(answer);
+
+		}
+	}
+
+}
