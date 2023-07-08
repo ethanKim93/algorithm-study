6f3b9afdb648ece88c876f03e1c98c4c01901680
김민현 <87460980+ethanKim93@users.noreply.github.com>
2023-07-09 01:06:23 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.23 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87377. 교점에 별 만들기/README.md
+++ 프로그래머스/lv2/87377. 교점에 별 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 교점에 별 만들기 - 87377 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377?language=java) 
 
 ### 성능 요약
 
-메모리: 76.9 MB, 시간: 0.24 ms
+메모리: 82.1 MB, 시간: 0.23 ms
 
 ### 구분
 
--- 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,74 +1,44 @@
 import java.util.*;
 
 class Solution {
+    
+    List<Point> points = new ArrayList<>();
+    
     public String[] solution(int[][] line) {
-        
-        List<Point> crossPoints = new ArrayList<Point>();
-        
-        for(int i = 0 ; i < line.length ; i ++){
-            int[] line1 = line[i];
-            for(int j = i ; j < line.length ; j ++){
-                int[] line2 = line[j];
-                Point point = crossLine(line1,line2,crossPoints);
-                if(point != null){
-                    crossPoints.add(point);
-                }
+        String[] answer = {};
+        for(int i = 0 ; i < line.length ; i++ ){
+            for(int j = i+1 ; j <line.length ; j ++){
+                Point inter =  intersection(line[i][0],line[i][1],line[i][2],
+                                            line[j][0],line[j][1],line[j][2]);
+                if(inter != null) points.add(inter);
             }
         }
-
-        Point maximum = getMaxPosition(crossPoints);
-        Point minimum = getMinPosition(crossPoints);
         
-
-        int height = (int) (maximum.y - minimum.y + 1);
-        int width = (int) (maximum.x - minimum.x + 1);
+        Point minimum = getMinimumPoint(points);
+        Point maximum = getMaximumPoint(points);
+        
+        int width = (int) (maximum.x - minimum.x+1);
+        int height = (int) (maximum.y - minimum.y+1);
         
         char[][] arr = new char[height][width];
         for(char[] row : arr){
             Arrays.fill(row,'.');
         }
-        // System.out.println(crossPoints.toString());
-        //         System.out.println(maximum);
-        // System.out.println(minimum);
-
-        for(Point p : crossPoints){
-            int x = (int)(p.x - minimum.x);
-            int y = (int)(maximum.y - p.y);
+        
+        for(Point p : points){
+            int x = (int) (p.x - minimum.x);
+            int y = (int) (maximum.y - p.y);
             arr[y][x] = '*';
         }
-
-        String[] answer = new String[arr.length];
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = new String(arr[i]);
-        }
-        return answer;
         
-        // return  new String[1];
-
+        String[] result = new String[arr.length];
+        for(int i = 0; i <result.length; i ++){
+            result[i] = new String(arr[i]);
+        }
+        return result;
     }
     
-    // 교차점 찾는 method
-    static Point crossLine(int[] line1 , int[] line2 , List<Point> crossPoints){
-        long A = line1[0];
-        long B = line1[1];
-        long E = line1[2];
-        
-        long C = line2[0];
-        long D = line2[1];
-        long F = line2[2];
-        
-   
-        
-        double x = (double) (B*F - E*D) / (A*D - B*C);
-        double y = (double) (E*C - A*F) / (A*D - B*C);
-
-        if(x%1 != 0 || y%1 !=0) return null;
-            
-        return new Point((long)x,(long)y);
-    } 
-    
-    // 최소 좌표 
-    static Point getMinPosition(List<Point> points){
+    private Point getMinimumPoint(List<Point> points){
         long x = Long.MAX_VALUE;
         long y = Long.MAX_VALUE;
         
@@ -76,36 +46,38 @@ class Solution {
             if(p.x < x) x = p.x;
             if(p.y < y) y = p.y;
         }
-        
         return new Point(x,y);
     }
-    
-     // 최대 좌표 
-    static Point getMaxPosition(List<Point> points){
+     private Point getMaximumPoint(List<Point> points){
         long x = Long.MIN_VALUE;
         long y = Long.MIN_VALUE;
         
+         
         for(Point p : points){
             if(p.x > x) x = p.x;
             if(p.y > y) y = p.y;
         }
-        
         return new Point(x,y);
     }
-  
+    // 두개의 교차를 구함
+    private Point intersection(long a1, long b1 ,long c1 ,long a2, long b2 ,long c2 ){
+        double x = (double)(b1*c2-b2*c1) / (a1*b2 -a2*b1);
+        double y = (double)(a2*c1-a1*c2) / (a1*b2 -a2*b1);
+        
+        if((x%1 != 0 || y%1 != 0)){
+            return null;
+        }
+        
+        return new Point((long)x,(long)y);
+    }
     
-    static class Point {
-        private long x;
-        private long y;
+    private static class Point{
+         public final long x, y ;
         
-        private Point(long x , long y){
-            this.x = x ; 
-            this.y = y ;
-        }
-        
-        @Override
-        public String toString() {
-            return String.format("[x = %s,y = %s]", x, y);
-        }
+         private Point(long x, long y){
+             this.x = x;
+             this.y = y;
+         }
+            
     }
 }
