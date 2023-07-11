697b712ada0ba6abe0785a2f8c7c7aa96e5a7f16
김민현 <87460980+ethanKim93@users.noreply.github.com>
2023-07-12 01:10:56 +0900

    [level 3] Title: 불량 사용자, Time: 4.89 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+
+class Solution {
+    private void count(int index , Set<String> banned,String[][] bans,Set<Set<String>> banSet){
+        if(index == bans.length){
+            banSet.add(new HashSet<>(banned));
+            return;
+        }
+        
+        for(String id : bans[index]){
+            if(banned.contains(id)) continue;
+            banned.add(id);
+            count(index + 1 ,banned,bans,banSet);
+            banned.remove(id);
+        }
+    }
+    public int solution(String[] user_id, String[] banned_id) {
+        int answer = 0;
+        String[][] bans = Arrays.stream(banned_id)
+            .map(banned -> banned.replace('*','.'))
+            .map(banned -> Arrays.stream(user_id)
+                .filter(id -> id.matches(banned))
+                .toArray(String[]::new))
+            .toArray(String[][]::new);
+        
+        Set<Set<String>> banSet = new HashSet<>();
+        count(0,new HashSet<>(),bans,banSet);
+        return banSet.size();
+    }
+}
+
+// import java.util.*;
+
+// class Solution {
+//     static boolean[] visit;
+//     static Set<Set<String>> set = new HashSet<>();
+//     public int solution(String[] user_id, String[] banned_id) {
+        
+//         visit = new boolean[user_id.length];
+        
+//         dfs(new HashSet<String>(),user_id,banned_id,0);     
+//         System.out.println(set);
+//         return set.size();
+//     }
+    
+//     void dfs(Set<String> banned, String[] user_id, String[] banned_id , int index){
+//         // 종료 조건 
+//         if(index == banned_id.length){
+//             System.out.println(set);
+//             set.add(banned);
+//             return;
+//         }
+        
+//         String ban = banned_id[index];
+//         for(int i = index+1 ; i < user_id.length ; i++){
+//             if(!visit[i] && isBan(user_id[i],ban)){
+//                 visit[i] = true;
+//                 banned.add(user_id[i]);
+//                 System.out.println(user_id[i]);
+//                 dfs(banned,user_id,banned_id,index+1);
+//                 banned.remove(user_id[i]);
+//                 visit[i] = false;
+//             }
+//         }
+//     }
+    
+//     static boolean isBan(String userId, String banId){
+        
+//         if(userId.length() != banId.length()){
+//             return false;
+//         }
+        
+//         for(int i = 0 ; i < userId.length() ; i ++){
+//             if(banId.charAt(i) != '*' && banId.charAt(i) != userId.charAt(i)){
+//                return  false;
+//             }
+//         }
+//         return true;
+//     }
+// }
