7132559dce13262ae7c1841654486c7632b0e624
김민현 <87460980+ethanKim93@users.noreply.github.com>
2023-07-22 00:07:49 +0900

    [level 2] Title: H-Index, Time: 0.47 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -6,12 +6,16 @@ class Solution {
         int answer = 0;
         
         for(int i = 0 ; i < citations.length; i++){
-        
-            int h = citations.length - i;   // 논문 개수
+//             int h = citations.length - i;   // 논문 개수
 
-            if(citations[i] >= h){
-                answer = h;
+//             if(citations[i] >= h){
+//                 answer = h;
+//                 break;
+//             }
+            if(citations.length - i <= citations[i] ){
+                answer = citations.length - i;
                 break;
+            
             }
         }
         return answer;
