cb19dbc177a6f21767b1bbb0ae036a8a9633e7f0
김민현 <87460980+ethanKim93@users.noreply.github.com>
2023-09-17 21:58:16 +0900

    [level 3] Title: 단어 변환, Time: 0.28 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Solution {
+    
+    private static class State{
+        public final String word; 
+        public final int step;
+        
+        private State(String word,int step){
+            this.word = word;
+            this.step = step;
+        }
+    }
+    
+    public boolean isConvertable(String src ,String dst){
+        char[] srcArr = src.toCharArray();
+        char[] dstArr = dst.toCharArray();
+        
+        int diff = 0 ;
+        for(int i = 0;i < srcArr.length;i++){
+            if(srcArr[i] != dstArr[i]) diff ++;
+        }
+        return diff == 1;
+    }
+    
+    public int solution(String begin, String target, String[] words) {
+        boolean[] isVisited = new boolean[words.length];
+        
+        Queue<State> queue = new LinkedList<>();
+        queue.add(new State(begin,0));
+        
+        while(!queue.isEmpty()){
+            State state = queue.poll();
+            
+            if(state.word.equals(target)){
+                return state.step;
+            }
+            
+            for(int i = 0; i < words.length; i++){
+                String next = words[i];
+                
+                if(isVisited[i]){
+                    continue;
+                }
+                
+                if(!isConvertable(state.word,next)){
+                    continue;
+                }
+                
+                isVisited[i] = true;
+                queue.add(new State(next,state.step+1));
+                
+            }
+            
+        }
+        int answer = 0;
+        return answer;
+    }
+}
