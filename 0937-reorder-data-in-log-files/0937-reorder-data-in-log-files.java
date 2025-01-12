eab847a1f5a7640c62e739da407d7b31d3eb2a1a
김민현 <87460980+ethanKim93@users.noreply.github.com>
2025-01-12 22:57:14 +0900

    Time: 5 ms (60.3%), Space: 44.6 MB (89.41%) - LeetHub

--- 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
@@ -1,100 +1,45 @@
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
-        List<String[]> letters = new ArrayList<>();
-        List<String[]> digits = new ArrayList<>();
 
-        // 문자 타입, 숫자 타입 구분
+        List<String> letters = new ArrayList<>();
+        List<String> digits = new ArrayList<>();
+
         for(String log : logs){
             String[] logArr = log.split(" ");
             char sample = logArr[1].charAt(0);
             if(Character.isLetter(sample)){
-                letters.add(logArr);
+                letters.add(log);
             }else{
-             digits.add(logArr);   
+             digits.add(log);   
             }
         }
 
         String[] result = new String[logs.length];
 
-        //문자열 정렬
-        for(int i = 0 ; i <letters.size() - 1; i++ ){
-            for(int j = 0; j < letters.size() - i - 1 ; j++){
-                String[] preLogs = letters.get(j);
-                String[] nextLogs = letters.get(j + 1);
 
+        letters.sort((s1,s2) -> {
 
-                StringBuilder preLogTail = new StringBuilder();
-                for(int k = 1 ; k < preLogs.length; k++){
-                    preLogTail.append(" ");
-                    preLogTail.append(preLogs[k]);
-                }
-                 
-                StringBuilder nextLogTail = new StringBuilder();
-                for(int k = 1 ; k < nextLogs.length; k++){
-                    nextLogTail.append(" ");
-                    nextLogTail.append(nextLogs[k]);
-                }
-                
-                if(preLogTail.toString().equals(nextLogTail.toString())){
-                    if(preLogs[0].compareTo(nextLogs[0]) > 0){
-                        String[] temp = letters.get(j);
-                        letters.set(j,letters.get(j+1)); 
-                        letters.set(j+1,temp); 
-                    }
-                }else if(preLogTail.toString().compareTo(nextLogTail.toString()) > 0){
-                    String[] temp = letters.get(j);
-                    letters.set(j,letters.get(j+1)); 
-                    letters.set(j+1,temp); 
-                }
+            String[] pre = s1.split(" ",2);
+            String[] next = s2.split(" ",2);
 
+            int compared = pre[1].compareTo(next[1]);
+
+            if(compared == 0){
+                return pre[0].compareTo(next[0]);
             }
-        }
 
-        // 숫자 정렬 
-        // for(int i = 0 ; i <digits.size() - 1; i++ ){
-        //     for(int j = 0; j < digits.size() - i - 1 ; j++){
-        //         String[] preLogs = digits.get(j);
-        //         String[] nextLogs = digits.get(j + 1);
-
-        //         int minSize = Math.min(preLogs.length,nextLogs.length);
-
-        //         for(int k = 1 ; k < minSize; k++){
-        //             int pL = Integer.parseInt(preLogs[k]);
-        //             int nL = Integer.parseInt(nextLogs[k]);
-        //             System.out.println(pL +" " + nL);
-        //             if(pL > nL){
-        //                 String[] temp = digits.get(j);
-        //                 digits.set(j,digits.get(j+1)); 
-        //                 digits.set(j+1,temp); 
-        //                 break;
-        //             }   
-        //         }
-                 
-        //     }
-        // }
-
+            return compared;
+        });
 
 
         for(int i = 0 ; i <letters.size() ; i++ ){
-            String[] letter = letters.get(i);
-            StringBuilder letterLog = new StringBuilder();
-            letterLog.append(letter[0]);
-            for(int k = 1 ; k < letter.length; k++){
-                letterLog.append(" ");
-                letterLog.append(letter[k]);
-            } 
-            result[i] = letterLog.toString();
+            String letter = letters.get(i);
+            result[i] = letter;
         }
 
         for(int i = 0 ; i < digits.size() ; i++ ){
-            String[] digit = digits.get(i);
-            StringBuilder digitLog = new StringBuilder();
-            digitLog.append(digit[0]);
-            for(int k = 1 ; k < digit.length; k++){
-                digitLog.append(" ");
-                digitLog.append(digit[k]);
-            } 
-            result[letters.size() + i] = digitLog.toString();
+            String digit = digits.get(i);
+            result[letters.size() + i] = digit;
         }
 
         return result;
