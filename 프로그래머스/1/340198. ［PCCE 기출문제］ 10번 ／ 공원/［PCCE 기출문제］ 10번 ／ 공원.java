56b13a0e02ff188b0db9a12a3bfeacc82a3a25b3
김민현 <87460980+ethanKim93@users.noreply.github.com>
2025-03-18 22:12:38 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 7.94 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] mats, String[][] park) {
+        int answer = -1;
+        
+        List<Integer> matList =  new ArrayList<>();
+        for(int i = 0 ; i < mats.length ; i++){
+            matList.add(mats[i]);
+        }
+        matList.sort((o1,o2) -> o1-o2);
+        
+        for(int i = 0 ; i < park.length ; i++){
+            for(int j = 0 ; j < park[i].length ; j++){   
+                List<Integer> removeMat = new ArrayList<>(); 
+                for(int k = 0 ; k < matList.size() ; k ++){
+                    int n = matList.get(k);
+                    boolean isCheck = true;
+                    for(int r = i ; r < i + n  ; r++){
+                        for(int c = j ; c < j + n  ; c++){
+                        
+                            if(r < 0 || r >= park.length || c < 0 || c >= park[r].length ){
+                                isCheck =false;
+                                break;
+                            }
+                            if(!"-1".equals(park[r][c])){
+                                isCheck =false;
+                                break;
+                            }
+                        }
+                        if(!isCheck){
+                            break;
+                        }
+                    }
+                    if(isCheck){
+                        removeMat.add(n);
+                        answer = Math.max(answer,n);
+                    }
+                }
+                matList.removeAll(removeMat);
+            }    
+        }
+        return answer;
+    }
+}
