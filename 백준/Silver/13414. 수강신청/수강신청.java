939c95036adb2f4f5d0e9e77b195bfe44642ab0f
김민현 <87460980+ethanKim93@users.noreply.github.com>
2025-03-28 09:50:04 +0900

    [Silver III] Title: 수강신청, Time: 580 ms, Memory: 65260 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.java
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -1,47 +1,31 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int K = Integer.parseInt(st.nextToken());
-		int L = Integer.parseInt(st.nextToken());
-		
-		String[] userArr = new String[L]; 
-		Map<String,Integer> lastIndexMap = new HashMap<>();
-		for(int i  = 0 ; i < L ; i ++) {
-			
-			String userId = br.readLine();
-			userArr[i] = userId;
-			if(lastIndexMap.containsKey(userId)) {
-				userArr[lastIndexMap.get(userId)] = null;
-				lastIndexMap.put(userId, i);
-			}else {
-				lastIndexMap.put(userId, i);
-			}
-			
-		}
-		
-		int index = 0;
-		while(K > 0 && index < L) {
-			if(userArr[index] != null) {
-				System.out.println(userArr[index]);
-				K --;
-			}
+        int K = Integer.parseInt(st.nextToken()); // 수강 가능 인원
+        int L = Integer.parseInt(st.nextToken()); // 대기 목록 길이
 
-			index ++;
-		}
-		
-	}
+        LinkedHashSet<String> students = new LinkedHashSet<>();
 
+        for (int i = 0; i < L; i++) {
+            String studentId = br.readLine();
+            students.remove(studentId); // 중복 제거 (가장 마지막 신청만 유효)
+            students.add(studentId);
+        }
+
+        // 결과 출력 (K명까지만)
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for (String id : students) {
+            sb.append(id).append("\n");
+            count++;
+            if (count == K) break; // K명까지만 출력
+        }
+
+        System.out.print(sb);
+    }
 }
